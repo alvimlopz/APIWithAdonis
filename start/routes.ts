f372caec793e2205e067ee605a2429e0be05810c
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-27 10:43:35 -0300

    Criando o controller

--- start/routes.ts
+++ start/routes.ts
@@ -24,4 +24,6 @@ import Route from '@ioc:Adonis/Core/Route'
   Route.get('/', async () => {
     return { hello: 'world' }
   })
+
+  Route.post('/moments', 'MomentsController.store')
  }).prefix('/api')

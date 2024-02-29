078492dcbd7055504858590f63999d2e6d164981
alvaro rodrigues <alvim.lopez@gmail.com>
2024-02-29 12:00:45 -0300

    API finalizada com sucesso

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -50,6 +50,8 @@ export default class MomentsController {
   public async show({ params }: HttpContextContract){
     const moment = await Moment.findOrFail(params.id)
 
+    await moment.load('comments')
+
     return {
       data: moment,
     }
